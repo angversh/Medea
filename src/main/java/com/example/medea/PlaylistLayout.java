1318b46fdc0bd3f6543401b43c0ef491ff6f4cb3
Asphodell <vladislaus.shabalin@yandex.ru>
2022-05-31 21:41:20 +0300

    the class "PlaylistLayout" was commented out to revise the architecture

--- src/main/java/com/example/medea/PlaylistLayout.java
+++ src/main/java/com/example/medea/PlaylistLayout.java
@@ -1,48 +1,48 @@
-package com.example.medea;
-
-import javafx.scene.image.Image;
-
-import java.util.Objects;
-
-public class PlaylistLayout {
-    private String name;
-    private String imageSrc;
-    private String artist;
-    private static String playlistCover;
-    private static String playlistName;
-    private static String playlistArtist;
-
-    public static void setData(PlaylistLayout playlistLayout) {
-        Image image = new Image(Objects.requireNonNull(PlaylistLayout.class.getResourceAsStream(playlistlayout.getImageSrc())));
-
-        playlistCover.setImage(image);
-        playlistName.setText(playlistlayout.getName());
-        playlistArtist.setText(playlistlayout.getArtist());
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageSrc() {
-        return imageSrc;
-    }
-
-    public void setImageSrc(String imageSrc) {
-        imageSrc = imageSrc;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-}
+//package com.example.medea;
+//
+//import javafx.scene.image.Image;
+//
+//import java.util.Objects;
+//
+//public class PlaylistLayout {
+//    private String name;
+//    private String imageSrc;
+//    private String artist;
+//    private static String playlistCover;
+//    private static String playlistName;
+//    private static String playlistArtist;
+//
+//    public static void setData(PlaylistLayout playlistLayout) {
+//        Image image = new Image(Objects.requireNonNull(PlaylistLayout.class.getResourceAsStream(playlistlayout.getImageSrc())));
+//
+//        playlistCover.setImage(image);
+//        playlistName.setText(playlistlayout.getName());
+//        playlistArtist.setText(playlistlayout.getArtist());
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getImageSrc() {
+//        return imageSrc;
+//    }
+//
+//    public void setImageSrc(String imageSrc) {
+//        imageSrc = imageSrc;
+//    }
+//
+//    public String getArtist() {
+//        return artist;
+//    }
+//
+//    public void setArtist(String artist) {
+//        this.artist = artist;
+//    }
+//
+//}
 
